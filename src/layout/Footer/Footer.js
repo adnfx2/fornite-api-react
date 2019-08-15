47a8a6382c6387ebf37f71924cd79a979926b58e
adnfx2 <fx.adrian@gmail.com>
2019-08-15 19:23:06 -0400

    footer added

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,23 +1,68 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import flaticon_svg from "../../assets/images/flaticon.svg";
+import { fornite_references, menu_links, contact_data } from "./footer_data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+
+const List = ({ renderItem, items }) => items.map(item => renderItem(item));
+
+const renderLinks = ({ type, label, content, faIcon }) => (
+  <li className="py-1">
+    {type === "string" ? (
+      <p className="m-0">
+        {faIcon ? (
+          <span className="mr-2">
+            <FontAwesomeIcon icon={faIcon} />
+          </span>
+        ) : (
+          ""
+        )}
+        <span>{content}</span>
+      </p>
+    ) : (
+      <a className="text-light" href={content}>
+        {faIcon ? (
+          <span className="mr-2">
+            <FontAwesomeIcon icon={faIcon} />
+          </span>
+        ) : (
+          ""
+        )}
+        <span>{label}</span>
+      </a>
+    )}
+  </li>
+);
+
+const footerSections = [
+  { title: "Fortnite official", items: fornite_references },
+  { title: "Menu", items: menu_links },
+  { title: "Contact", items: contact_data }
+];
 
 const Footer = props => {
   return (
-    <footer style={{ overflow: "hidden" }} className="pt-5 bg-dark">
+    <footer className="pt-5 bg-dark text-light small">
       <Container>
         <Row>
+          {footerSections.map(({ title, items }) => (
+            <Col xs={12} md={4}>
+              <h5>{title}</h5>
+              <ul className="list-unstyled">
+                <List renderItem={renderLinks} items={items} />
+              </ul>
+            </Col>
+          ))}
+        </Row>
+        <Row className="pt-3 pb-3">
           <Col>
-            <ul>
-              <li>Link</li>
-              <li>Link</li>
-              <li>Link</li>
-            </ul>
-          </Col>
-          <Col>
-            <div>
-              Icons made by{" "}
+            <hr className="m-0 pb-3" />
+            <div className="text-center">
+              This website is made with <FontAwesomeIcon icon={faReact} /> by
+              @adnfx2. Icons made by{" "}
               <a
+                className="text-white-50"
                 href="https://www.flaticon.com/authors/freepik"
                 title="Freepik"
               >
@@ -25,10 +70,11 @@ const Footer = props => {
               </a>{" "}
               from{" "}
               <a href="https://www.flaticon.com/" title="Flaticon">
-                <img src={flaticon_svg} alt="Flaticon icon" width="100px"/>
+                <img src={flaticon_svg} alt="Flaticon icon" width="75px" />
               </a>{" "}
               licensed by{" "}
               <a
+                className="text-white-50"
                 href="http://creativecommons.org/licenses/by/3.0/"
                 title="Creative Commons BY 3.0"
                 target="_blank"
--- src/layout/Footer/footer_data.js
+++ src/layout/Footer/footer_data.js
@@ -1,1 +1,79 @@
-export const elinks = [{ tag: "github", url: "" }];
+import {
+  faTwitch,
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+
+import {
+  faExternalLinkAlt,
+  faEnvelope,
+  faPhone
+} from "@fortawesome/free-solid-svg-icons";
+
+export const contact_data = [
+  {
+    type: "string",
+    label: "Email",
+    content: "fx.adrian@gmail.com",
+    faIcon: faEnvelope
+  },
+  {
+    type: "string",
+    label: "Phone number",
+    content: "+58-412-7665245",
+    faIcon: faPhone
+  },
+  {
+    type: "url",
+    label: "LinkedIn",
+    content: "www.google.com",
+    faIcon: faLinkedin,
+    external: true
+  }
+];
+
+export const menu_links = [
+  { type: "url", label: "Home", content: "#" },
+  { type: "url", label: "Items", content: "#" },
+  { type: "url", label: "Blog", content: "#" }
+];
+
+export const fornite_references = [
+  {
+    type: "url",
+    label: "Fortnite",
+    content: "https://www.epicgames.com/fortnite",
+    faIcon: faExternalLinkAlt,
+    external: true
+  },
+  {
+    type: "url",
+    label: "Get fortnite now!",
+    content: "https://www.epicgames.com/fortnite/en-US/download",
+    faIcon: faExternalLinkAlt,
+    external: "true"
+  },
+  {
+    type: "url",
+    label: "Facebook",
+    content: "https://www.facebook.com/FortniteGame/",
+    faIcon: faFacebook,
+    external: "true"
+  },
+  {
+    type: "url",
+    label: "Twitter",
+    content: "https://twitter.com/FortniteGame",
+    faIcon: faTwitter,
+    external: "true"
+  },
+  {
+    type: "url",
+    label: "Twitch",
+    content: "https://www.twitch.tv/fortnitegame",
+    faIcon: faTwitch,
+    external: "true"
+  }
+];
