76ce086ef179c670f4afd5ece9b8cd753ddea96c
adnfx2 <fx.adrian@gmail.com>
2019-08-24 17:40:54 -0400

    weaponsSchema added for normalizr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import configureStore from "./redux/store/configureStore";
-import App from "./layout/App/App";
-import "./styles/index.css";
+import { testApi } from "./redux/middlewares/CallFortniteAPI";
 
-const store = configureStore();
+testApi();
 
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import configureStore from "./redux/store/configureStore";
+// import App from "./layout/App/App";
+// import "./styles/index.css";
+//
+// const store = configureStore();
+//
+// ReactDOM.render(<App store={store} />, document.getElementById("root"));
--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -2,10 +2,10 @@
 export const API_ROOT = "https://fortnite-api.theapinetwork.com";
 export const AUTH_TOKEN = "8231bb87427bdaa20e08de2767f9992e";
 // endpoints
-export const WEAPONS = "/weapons/get"; // headers: {{Authorization}}
-export const ITEMS = "/items/list"; // headers: {{Authorization}}
-export const NEWS_STW = "/stw_motd/get";
-export const NEWS_BR = "/br_motd/get";
+export const ENDPOINT_WEAPONS = "weapons/get"; // headers: {{Authorization}}
+export const ENDPOINT_ITEMS = "items/list"; // headers: {{Authorization}}
+export const ENDPOINT_NEWS_STW = "stw_motd/get";
+export const ENDPOINT_NEWS_BR = "br_motd/get";
 
 // dynamic endpoints
 export const selectItemById = itemid => `/item/get?id=${itemid}`; // headers: {{Authorization}} param: id {{itemid}}
