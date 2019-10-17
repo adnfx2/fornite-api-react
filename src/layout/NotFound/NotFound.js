ddfb19965b68f54ed894b3ab6387ee08a1a72604
adnfx2 <fx.adrian@gmail.com>
2019-10-17 18:04:01 -0400

    NotFound styles added

--- src/layout/NotFound/NotFound.js
+++ src/layout/NotFound/NotFound.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
+import rocketingOutImage from "../../assets/images/fortnite-rocketing.jpg";
+const useStyles = createUseStyles({
+  "not-found": {
+    height: "100vh",
+    backgroundColor: "#999",
+    backgroundSize: "cover",
+    backgroundImage: `url(${rocketingOutImage})`
+  },
+  title: {
+    composes: ["pt-5 pl-3"],
+    fontFamily: "var(--fortnite-font)",
+    color: "#fff",
+    textShadow: "2px 2px 8px #212529"
+  }
+});
 
-const NotFound = props => <div>Not Found</div>;
+const NotFound = props => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles["not-found"]}>
+      <h1 className={styles.title}>Page not found, RUN!!!</h1>
+    </div>
+  );
+};
 
 export default NotFound;
